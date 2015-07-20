bd44ba60cdd552c8c39bf2c2f509a350acb4096c
intel-realsense <wizard.oz@intel.com>
2015-07-21 01:46:02 +0300

    test

--- webpage-content/realsense/js/realsense-detection.js
+++ webpage-content/realsense/js/realsense-detection.js
@@ -11,29 +11,28 @@ function ValidatePlatform() {
      
         
         if (info.nextStep == 'ready') {
-            
             //good to go
             console.log("realsense good");
-             $("#platform-detection").hide();
+            $("#platform-detection").hide();
             $("#platform-ready").show(200);
           
         }
         else if (info.nextStep == 'unsupported') {
             console.warn(' Platform is not supported for Intel(R) RealSense(TM) SDK: either you are missing the required camera, or your OS and browser are not supported ');
-             $("#platform-detection").hide();
+            $("#platform-detection").hide();
             
         } else if (info.nextStep == 'driver') {
-            console.warn('Please upgrade RealSense(TM) Depth Camera Manager (DCM) and firmware before running the application  http://www.intel.com/realsense  ');
-             $("#platform-detection").hide();
-             $("#platform-dcm-missing").show(200);
+            console.warn('Please upgrade RealSense(TM) Depth Camera Manager (DCM) and firmware before running the application  http://www.intel.com/realsense ');
+            $("#platform-detection").hide();
+            $("#platform-dcm-missing").show(200);
             
          
-        } else if (info.nextStep == 'runtime') {
+        } /*else if (info.nextStep == 'runtime') {
             console.warn('please download and install runtime exe');
-             $("#platform-detection").hide();
+            $("#platform-detection").hide();
             $("#platform-runtime-missing").show(200);
            
-        }
+        }*/
 
     }).catch(function (error) {
          console.warn('other unknown failure. '+ JSON.stringify(error));
