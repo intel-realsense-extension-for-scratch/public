ce5aef275c9a0089314f4e37c453ec871decfb39
intel-realsense <wizard.oz@intel.com>
2015-08-12 10:10:34 +0300

    updated sample adding new index that refer to our site

--- extension/analytics.js
+++ extension/analytics.js
@@ -1,11 +1,19 @@
- (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+(function(i,s,o,g,r,a,m){
+    i['GoogleAnalyticsObject']=r;
+    i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)
+    },i[r].l=1*new Date();
+    a=s.createElement(o), m=s.getElementsByTagName(o)[0];
+    a.async=1;a.src=g;
+    m.parentNode.insertBefore(a,m)
   })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-  ga('create', 'UA-31647162-2', 'auto');
+
+  ga('create', 'UA-66218400-1', 'auto');
   ga('send', 'pageview');
+
 
 
 //add functionality for usage of each block
 
-// https://developers.google.com/analytics/devguides/collection/gajs/eventTrackerGuide
+//https://developers.google.com/analytics/devguides/collection/gajs/eventTrackerGuide
+
