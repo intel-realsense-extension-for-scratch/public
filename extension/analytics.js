819cdd8c77cac9ec75a73de98d825623b7c39e4a
intel-realsense <wizard.oz@intel.com>
2015-08-12 13:53:54 +0300

    replace http with http

--- extension/analytics.js
+++ extension/analytics.js
@@ -6,7 +6,7 @@
     a=s.createElement(o), m=s.getElementsByTagName(o)[0];
     a.async=1;a.src=g;
     m.parentNode.insertBefore(a,m)
-  })(window,document,'script','http://www.google-analytics.com/analytics.js','ga');
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
 
   ga('create', 'UA-66218400-1', 'auto');
   ga('send', 'pageview');
