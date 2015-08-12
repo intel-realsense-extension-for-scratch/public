464f89e5986d9d637a6e352de5e924345aba0c55
intel-realsense <wizard.oz@intel.com>
2015-08-12 14:02:12 +0300

    ga1

--- extension/analytics-extension.js
+++ extension/analytics-extension.js
@@ -2,10 +2,10 @@
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','http://www.google-analytics.com/analytics.js','ga');
+  })(window,document,'script','http://www.google-analytics.com/analytics.js','ga1');
 
-  ga('create', 'UA-66218400-2', 'auto');
-  ga('send', 'pageview');
+  ga1('create', 'UA-66218400-2', 'auto');
+  ga1('send', 'pageview');
 
 
 
