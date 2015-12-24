951bb5c3059c2e552953c693d6a33f4b4273c044
intel-realsense-extension-for-scratch <wizard.oz@intel.com>
2015-12-24 11:36:18 +0200

    new dialog message

--- extension/intel_realsense_extension.js
+++ extension/intel_realsense_extension.js
@@ -991,7 +991,8 @@ accordance with the terms of that agreement
             
         if (rsd.Status.status == 0) {
             
-            showModal("template-realsense");
+            showModal("template-realsense", {title='Hmm, something is wrong :) ...' , 
+                                             message='See more information on the extension website. <a href="www.google.com">go here</a>'});
         }
     };
     
