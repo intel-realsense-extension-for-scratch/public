2eb68d5c414097a0386ea6d4c590c1e20df98999
intel-realsense-extension-for-scratch <wizard.oz@intel.com>
2016-01-10 14:13:19 +0200

    loading realsense.js faster

--- extension/intel_realsense_extension.js
+++ extension/intel_realsense_extension.js
@@ -1108,7 +1108,10 @@ accordance with the terms of that agreement
         .done(function(script, textStatus) {
 
             //$.getScript('https://cdn.rawgit.com/intel-realsense-extension-for-scratch/resources/master/intel/realsense.js')
-            $.getScript('https://raw.githubusercontent.com/intel-realsense-extension-for-scratch/resources/master/intel/realsense.js')
+        // dev link: https://rawgit.com/shacharoz/
+        // production link (cached): https://cdn.rawgit.com/shacharoz/
+
+     $.getScript('https://rawgit.com/intel-realsense-extension-for-scratch/resources/master/intel/realsense.js')
             .done(function(script, textStatus) {
              
                 dependencyAllCreated();
